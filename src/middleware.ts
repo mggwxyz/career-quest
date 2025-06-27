a2e8eb022cbb6bfc7dd21233814e8157b08080a2
Michael Gilbertson <mggwxyz@gmail.com>
2025-06-26 20:13:32 -0400

    Added shadcn and supbase ui components

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,20 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-// const isProtectedRoute = createRouteMatcher([
-//   '/intake/:path*',
-//   '/careers/:path*',
-// ])
-
-// export default clerkMiddleware(async (auth, req) => {
-//   if (isProtectedRoute(req)) await auth.protect()
-// })
-
+import { updateSession } from '@/lib/supabase/middleware'
 import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
 }
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - api/ (API routes - allow without authentication)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|api|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
