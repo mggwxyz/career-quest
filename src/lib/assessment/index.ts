dd3767c31ab3d97c527697ebf2b1f599ede57b37
Michael Gilbertson <mggwxyz@gmail.com>
2026-04-18 15:11:16 -0400

    feat(assessment): public re-exports

---
+++ src/lib/assessment/index.ts
@@ -0,0 +1,11 @@
+export * from './types'
+export { initialPosterior, updatePosterior } from './posterior'
+export { rankRiasec, hollandCode, confidenceBand, contextLean, buildResult } from './scoring'
+export { detectInconsistency } from './inconsistency'
+export { validateBank, eligibleItems, unseenItems, RIASEC_COVERAGE_FLOOR, DESIRABILITY_GAP_MAX } from './itemBank'
+export {
+  scoreItemForSelection, pickNextItem, pickWithCoveragePhase, shouldStop,
+  startSession, advance, finalize, chooseFirstItem,
+  FLOOR_ITEMS, CAP_ITEMS,
+} from './engine'
+export { formatResultForPrompt } from './promptFormat'
